4a66cfb2e337312522f33d1046fbac8303397a38
Ryan Hess <ryanmhess@gmail.com>
2022-09-16 09:20:36 -0500

    updated formatting of items

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -2,17 +2,17 @@
 
 function ListItem({ shoppingList }) {
     return (
-        <ul>
+        <>
             {shoppingList.map(item => {
                 return (
-                    <>
+                    <ul key={item.id}>
                         <li>{item.name}</li>
                         <li>{item.quantity}</li>
                         <li>{item.units}</li>
-                    </>
+                    </ul>
                 )
             })}
-        </ul>
+        </>
     )
 }
 
